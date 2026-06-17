d50fc098a4ea0bf88771fda042db41a8978fae8c
nejlayadav <nejlayadav@gmail.com>
2026-06-17 17:32:19 +0530

    first commit

---
+++ bill.js
@@ -0,0 +1,28 @@
+var unit=45;
+var bill =0; 
+var a,b,c,x;
+if(unit<50){
+    bill = unit*0.50;
+    unit=0
+}
+if(unit>=50){
+   a=Number(50*0.50);
+   unit-=50;
+   bill += a;
+}
+if (unit<100){
+    x = Number(unit * 0.75);
+    unit=0;
+    bill += x;
+}
+if (unit>=100){
+    b=Number(unit*0.75);
+    unit-=100;
+    bill += b;
+}
+if (unit>0){
+    c = Number(unit* 1.20);
+    bill += c;
+}
+
+console.log("total bill amount",Number(bill*1.2));
